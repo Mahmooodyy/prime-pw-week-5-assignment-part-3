511adb7d3c163a148069fd40352789e108a866b8
Mahmoud Mahmoud <99694897+Mahmooodyy@users.noreply.github.com>
2022-06-02 15:58:29 -0500

    made collection array, addToMyCollection function and the object

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,15 @@
 console.log('***** Music Collection *****')
+
+let collection = []
+
+function addToCollection(title, artist, yearPublished){
+    return myMusic
+
+}
+
+const myMusic = {
+title: "Bylaw EP",
+artist: "Martin Garrix",
+yearPublished: "2018"
+
+}
