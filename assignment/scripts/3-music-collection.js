cb3258756be78ad180070b86d2a72012dece7ee8
Mahmoud Mahmoud <99694897+Mahmooodyy@users.noreply.github.com>
2022-06-03 01:29:12 -0500

    got the framework done for the showCollection function just tryiing to get it to work.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,49 +3,44 @@ console.log('***** Music Collection *****')
 let collection =[]
 let food =['potato']
 
-function addToCollection0(title, artist, yearPublished) {
-const album = new Object ();
-album.title = title
-album.artist = artist
-album.yearPublished = yearPublished
-collection.push(album)
-console.log(album);
-return album
+function addToCollection(title, artist, yearPublished) {
+    const album = new Object ();
+    album.title = title
+    album.artist = artist
+    album.yearPublished = yearPublished
+    collection.push(album)
+    console.log(album);
+    return album
 };
 
-addToCollection0('Bylaw','Martin Garrix', '2018')
-addToCollection0('The Human Condition', 'Jon Bellion', '2016')
-addToCollection0('Cypress Grove', 'Glaive', '2020')
-addToCollection0('Glory Sound Prep ', 'Jon Bellion', '2018')
-addToCollection0('DAMN.', 'Kendrick Lamar', '2017')
-addToCollection0('bb u ok?', 'San Holo', '2021')
+addToCollection('Bylaw','Martin Garrix', '2018')
+addToCollection('The Human Condition', 'Jon Bellion', '2016')
+addToCollection('Cypress Grove', 'Glaive', '2020')
+addToCollection('Glory Sound Prep ', 'Jon Bellion', '2018')
+addToCollection('DAMN.', 'Kendrick Lamar', '2017')
+addToCollection('bb u ok?', 'San Holo', '2021')
 
-
-// I noticed that there are two ways to do this is one way more used or more correct than the other?
-
-
-function addToCollection1(title, artist, yearPublished) {
-this.title = title
-this.artist = artist
-this.yearPublished = yearPublished
-collection.push(this)
-console.log(this);
-return this
-};
-
-new addToCollection1('Bylaw','Martin Garrix', '2018')
-new addToCollection1('The Human Condition', 'Jon Bellion', '2016')
-new addToCollection1('Cypress Grove', 'Glaive', '2020')
-new addToCollection1('Glory Sound Prep ', 'Jon Bellion', '2018')
-new addToCollection1('DAMN.', 'Kendrick Lamar', '2017')
-new addToCollection1('bb u ok?', 'San Holo', '2021')
-
+console.log(collection);
 
 function showCollection(array) {
-    array
+
     console.log(array.length);
-    for (let i = 0; i < array.length; i++) {
-        console.log(addToCollection.title, 'By:', addToCollection.artist, 'Published in:', addToCollection.yearPublished);
+
+    for (let i = 0; i < array.length; i++){
+
+        console.log(`${title} By: ${artist} Published in: ${yearPublished}`);  
+
+    }
+    
+}
+showCollection(collection)
+
+function findByArtist(artist){
+    let sameArtist = []
+    for (let i = 0; i < collection.length; i++) {
+        if (condition) {
+            
+        }
         
     }
 }
